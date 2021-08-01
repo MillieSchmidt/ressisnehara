334bb7b92c523415edbf40c9f5d3775cc85451ad
Millie Schmidt <millenium2124@gmail.com>
2021-08-01 10:39:12 +0300

    initial commit for ressis nehara website

---
+++ blog-posts.js
@@ -0,0 +1,5 @@
+export const POSTS = [
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea nisi ipsa velit quod, nobis voluptatibus voluptatem laudantium omnis accusantium!',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea nisi ipsa velit quod, nobis voluptatibus voluptatem laudantium omnis accusantium!',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea nisi ipsa velit quod, nobis voluptatibus voluptatem laudantium omnis accusantium!'
+];
---
+++ modal-script.js
@@ -0,0 +1,3 @@
+document.getElementById('cancel').addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'none';
+});

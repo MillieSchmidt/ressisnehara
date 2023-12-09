29ad10e641e549002919a7fd377dd6edf310676b
Millie Schmidt <65649134+MillieSchmidt@users.noreply.github.com>
2023-12-09 18:40:20 +0200

    Delete blog-posts.js

--- blog-posts.js
+++
@@ -1,5 +0,0 @@
-export const POSTS = [
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea nisi ipsa velit quod, nobis voluptatibus voluptatem laudantium omnis accusantium!',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea nisi ipsa velit quod, nobis voluptatibus voluptatem laudantium omnis accusantium!',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea nisi ipsa velit quod, nobis voluptatibus voluptatem laudantium omnis accusantium!'
-];
